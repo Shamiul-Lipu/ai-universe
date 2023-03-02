0fc2e33e2fbdee24217d2b6afd6cdb5dec8fe926
unknown <shamiullipu@gmail.com>
2023-03-02 22:49:03 +0600

    added and displayed all dataa

--- app.js
+++ app.js
@@ -9,36 +9,48 @@ const loadData = async () => {
 // display all data
 const displayData = (dataList) => {
     console.log(dataList);
+    const cardContainer = document.getElementById('card-container');
+    dataList.forEach(data => {
+        const { id, name, features, published_in, image } = data;
+        cardContainer.innerHTML += `     
+
+        <div class="col">
+            <div class="card h-100 shadow p-3 mb-5 bg-body rounded">
+                <img src="${image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Features</h5>
+                    <p class="card-text">
+                        <!-- card details -->
+                    <ul id="features-${id}" class="list-unstyled features-ul">
+        
+                    </ul>
+                    <hr>
+                    <!-- date and modal details -->
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <h5>${name}</h5>
+                            <p><i class="bi bi-calendar4-week px-1"></i>${published_in}</p>
+                        </div>
+                        <button onclick="getDetails('${id}')" class="rounded rounded-circle  border-0 text-danger h3"><i
+                                class="bi bi-arrow-right-short"></i></button>
+                    </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    
+        `
+        let countFeatures = 1;
+        console.log(features);
+        const ul = document.getElementById(`features-${id}`)
+        features.forEach(feature => {
+            ul.innerHTML += `<li>${countFeatures++}. ${feature}</li>`;
+            console.log(feature)
+        });
+    });
 }
+
 
 
 // invoking all data
 loadData();
-
-
-/* 
-
-<div class="col ">
-    <div class="card shadow p-3 mb-5 bg-body rounded">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${Card title}</h5>
-            <ul class="list-unstyled">
-                <li><span>${1 count}</span>${list....}</li>
-            </ul>
-            <hr>
-            <!-- date and details -->
-            <div class="d-flex justify-content-between align-items-center">
-                <div>
-                    <h5>you chat 2.0</h5>
-                    <p><i class="bi bi-calendar4-week"></i></p>
-                </div>
-                <button class="rounded rounded-circle  border-0 text-danger h3"><i
-                        class="bi bi-arrow-right-short"></i></button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
- */
